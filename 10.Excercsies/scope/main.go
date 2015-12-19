585508279f554e4ba1d88e41ef224e154e113257
fangher <fangher@users.noreply.github.com>
2015-12-18 18:50:41 -0800

    Exercise 10

---
+++ 10.Excercsies/scope/main.go
@@ -0,0 +1,10 @@
+package main
+import (
+	 "github.com/fangher/CSCI191TAdvWebProgramming/10.Excercsies/scope/names"
+	 "fmt"
+)
+
+func main () {
+	//fmt.Println(names.MyName)
+	fmt.Println(names.yourName)
+}
