6b48b169ffe701b7fef921259b788f1cf39dfb29
fangher <fangher@users.noreply.github.com>
2015-12-19 14:55:38 -0800

    Front End

--- Web_Project/01_Front_End/main.go
+++ Web_Project/01_Front_End/main.go
@@ -43,57 +43,5 @@ func profile(res http.ResponseWriter, req *http.Request, ps httprouter.Params) {
 	templ.ExecuteTemplate(res, "profile.html", nil)
   }
   
-func profilename(res http.ResponseWriter, req *http.Request, ps httprouter.Params) {
-	sd := sessionInfo(req)
-	var user User
-	user.Username = ps.ByName("name")
-	if user.Username != sd.Username {
-		ctx := appengine.NewContext(req)
-		key := datastore.NewKey(ctx, "Users", user.Username, 0, nil)
-		err := datastore.Get(ctx, key, &user)
-		if err != nil {
-			panic(err)
-		}
-	} else {
-		user = sd.User
-	}
-
-}
-
-func checkName(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
-	ctx := appengine.NewContext(req)
-	bs, err := ioutil.ReadAll(req.Body)
-	sbs := string(bs)
-	log.Infof(ctx, "REQUEST BODY: %v", sbs)
-	var user User
-	key := datastore.NewKey(ctx, "Users", sbs, 0, nil)
-	err = datastore.Get(ctx, key, &user)
-	log.Infof(ctx, "ERR: %v", err)
-	if err != nil {
-		fmt.Fprint(res, "false")
-		return
-	} else {
-		fmt.Fprint(res, "true")
-	}
-}
-
-func checkEmail(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
-	ctx := appengine.NewContext(req)
-	bs, err := ioutil.ReadAll(req.Body)
-	sbs := string(bs)
-	q := datastore.NewQuery("Users").Filter("Email =", sbs)
-	var u []User
-	_, err = q.GetAll(ctx, &u)
-	if err != nil {
-		panic(err)
-	}
-	if len(u) > 0 {
-		fmt.Fprint(res, "true")
-	} else {
-		fmt.Fprint(res, "false")
-		return
-	}
-}
-
 
 }
