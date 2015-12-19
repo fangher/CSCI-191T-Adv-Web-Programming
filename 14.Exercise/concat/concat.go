18bc771d1c06ff559f13bf8515f20b55ec3e0d40
fangher <fangher@users.noreply.github.com>
2015-12-18 22:08:05 -0800

    Exercise 14

---
+++ 14.Exercise/concat/concat.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+
+func main () {
+	day := "Wednesday"
+	weather := "rainy"
+	newWeather := "sunny"
+
+	fmt.Println ("Today is " + day + " " + weather)
+	fmt.Println ("Next week will be " + day + " "  + newWeather)
+}
